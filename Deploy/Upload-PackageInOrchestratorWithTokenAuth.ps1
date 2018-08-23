6fd202b6d562c4bf6b533b12a76e5b074e69c3b4
UIPATH\bogdan.popescu <bogdan.popescu@uipath.com>
2018-08-23 19:22:26 +0100

    Adding script to publish packages using Orchestrator Cmdlets library and with token authentication.

---
+++ Deploy/Upload-PackageInOrchestratorWithTokenAuth.ps1
@@ -0,0 +1,41 @@
+﻿function UploadPackageInOrchestratorWithTokenAuth
+{
+	[CmdletBinding()]
+	param 
+	(
+		[Parameter(Mandatory = $true,
+				ValueFromPipeline = $true,
+		HelpMessage = 'Orchestrator URL')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$OrchestratorURL,
+        [Parameter(Mandatory = $true,
+				ValueFromPipeline = $true,
+		HelpMessage = 'User Name')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$UserName,
+        [Parameter(Mandatory = $true,
+				ValueFromPipeline = $true,
+		HelpMessage = 'Password')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Password,
+        [Parameter(Mandatory = $false,
+				ValueFromPipeline = $true,
+		HelpMessage = 'TenantName')]
+		[string]
+		$TenantName,
+        [Parameter(Mandatory = $false,
+				ValueFromPipeline = $true,
+		HelpMessage = 'Package file')]
+		[string]
+		$PackageFile
+	)
+
+    #acquire token
+    $token = Get-UiPathAuthToken -URL $OrchestratorURL -Username $UserName -Password $Password -TenantName $TenantName 
+   
+    #upload package
+    Add-UiPathPackage -PackageFile $PackageFile -AuthToken $token
+}
